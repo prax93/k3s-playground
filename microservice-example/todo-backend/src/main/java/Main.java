e44c5d15a7e6b24c3c695ff90a680425e319737e
prax93 <shana.sri@gmail.com>
2025-11-24 22:36:10 +0100

    Import Repository into Main Class

--- microservice-example/todo-backend/src/main/java/Main.java
+++ microservice-example/todo-backend/src/main/java/Main.java
@@ -1,6 +1,19 @@
+import model.User;
+import repository.Repository;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class Main {
 
-    public static void main(String[] args){
+    static void main() throws SQLException {
         System.out.println("Hello World");
+
+        Repository repository = new Repository();
+
+        ArrayList<User> users = repository.getUsers();
+
+        System.out.print(users);
+
     }
 }
